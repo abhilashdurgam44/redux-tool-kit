c484528d2711309e1320d845931b4f068a01e175
abhilashdurgam44 <studyece44@gmail.com>
2024-02-20 13:07:41 -0800

    firsst

---
+++ src/Store/counterSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+ const counterSlice=createSlice({
+
+     name:"counter",
+     initialState:0,
+
+     reducers:{
+
+        add:(state)=>state+1,
+        remove:(state)=>state-1
+     }
+ });
+
+ export const {add,remove}=counterSlice.actions;
+
+ export default counterSlice.reducer;

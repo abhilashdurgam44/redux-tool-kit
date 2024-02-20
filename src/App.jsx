114c63524168d55117405dc60ca9b035c248a574
abhilashdurgam44 <studyece44@gmail.com>
2024-02-20 13:26:48 -0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-
-import { useSelector } from 'react-redux'
-import './App.css'
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+import { add, remove } from './Store/counterSlice';
 
 function App() {
+  // Use useSelector to select the counter state from the Redux store
+  const count = useSelector((state) => state.counter);
 
-  const count=useSelector((state)=>state.counter)
- 
+  const dispatch=useDispatch();
 
   return (
     <>
-       <h1>this is a website:{count}</h1>
+      <h1>This website: {count}</h1>
+      <button onClick={()=>dispatch(add())}>add</button>
+      <button onClick={()=>dispatch(remove())}>remove</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Store/store.js
+++ src/Store/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
  import counterReducer from './counterSlice'
 
-        configureStore({
+        const store=configureStore({
 
             reducer:{
                 counter:counterReducer,
